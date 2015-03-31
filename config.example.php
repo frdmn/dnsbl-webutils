5d669e7c92ed3e739addc058e4bf70162ac940d5
Jonas Friedmann <j@frd.mn>
2015-03-31 07:40:07 +0200

    Basic configuration file

---
+++ config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* This is the general configuaration file for dnsbl-monitor.
+**/
+
+date_default_timezone_set("Europe/Berlin"); // Set default timezone for date()
+
+// General
+
+$settings['title'] = "DNSBL Monitor";
+
+// Design
+
+$theme['navbar-dark'] = false; // Enable dark theme for the navbar
+
+?>
