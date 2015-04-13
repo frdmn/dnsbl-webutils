7834b60ee719379421fa3291bb93be2843830417
Jonas Friedmann <j@frd.mn>
2015-04-13 11:50:34 +0200

    Togglable jumbotron and explanation

--- views/check.php
+++ views/check.php
@@ -1,10 +1,21 @@
       <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1><?= $settings['title'] ?></h1>
-        <p>A DNS-based Blackhole List (<i>DNSBL</i>) or Real-time Blackhole List (<i>RBL</i>) is an effort to stop email spamming. Most mail server software can be configured to reject or flag messages which have been sent from a site listed on one or more such lists.</p>
-        <p>Below you can <a class="a-tooltip" href="#" data-toggle="tooltip" data-original-title="Default tooltip">mass test</a> such <i>DNSBL</i>'s against the IPs of your mail servers to possible listings.</p>
-      </div><!--/.jumbotron -->
-
+      <?php
+        if ($theme['jumbotron']) {
+      ?>
+        <div class="jumbotron">
+          <h1><?= $settings['title'] ?></h1>
+          <?php
+            if ($theme['explanation']) {
+          ?>
+            <p>A DNS-based Blackhole List (<i>DNSBL</i>) or Real-time Blackhole List (<i>RBL</i>) is an effort to stop email spamming. Most mail server software can be configured to reject or flag messages which have been sent from a site listed on one or more such lists.</p>
+            <p>Below you can <a class="a-tooltip" href="#" data-toggle="tooltip" data-original-title="Default tooltip">mass test</a> such <i>DNSBL</i>'s against the IPs of your mail servers to possible listings.</p>
+          <?php
+            }
+          ?>
+        </div><!--/.jumbotron -->
+      <?php
+        }
+      ?>
       <div class="alert alert-warning alert-dismissible alert-hint" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         Enter the IP address or hostname of your mail server below and hit <strong>Check</strong>.
