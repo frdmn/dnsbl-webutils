148a4c835de81ca90c772233217d6cb456691694
Jonas Friedmann <j@frd.mn>
2015-04-14 20:05:31 +0200

    Bring back modernizr
    (resolve #4)

--- index.php
+++ index.php
@@ -96,6 +96,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/modernizr.js"></script>
     <script src="assets/js/build.js"></script>
   </body>
 </html>
