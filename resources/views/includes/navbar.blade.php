5d3631fd64bd9f54d23f63d69f1aec5457b7b338
Jonas Friedmann <j@frd.mn>
2015-04-21 16:11:09 +0200

    Tweak navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">DNSBL</a>
+      <a class="navbar-brand" href="/">DNSBL</a>
     </div><!--/.navbar-header -->
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
