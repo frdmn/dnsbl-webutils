484e26fd645b3c552fe7d936684e5cfdbb9f1f35
Jonas Friedmann <j@frd.mn>
2015-04-23 22:07:18 +0200

    Improve API view

--- resources/views/pages/api.blade.php
+++ resources/views/pages/api.blade.php
@@ -3,15 +3,16 @@
   <h1 class="page-header">API</h1>
   <p>Below you can find a few API calls that you can use to interact and communicate with <strong>dnsbl-webuitls</strong>.</p>
 
-  <h3 class="page-header">Check a hostname against a DNSBL <small>(Work in progress!)</small></h3>
+  <h3 class="page-header">Check a hostname against one specific DNSBL <small>(Work in progress!)</small></h3>
   <div>
     <div class="zero-clipboard"><span class="btn-clipboard">Request</span></div>
     <div class="bs-example bs-example-get">
-      <pre>/api/v1/check?host=<strong>[hostname.tld]</strong>&dnsbl=<strong>[dnsbl.host.tld]</strong></pre>
-      <dl class="dl-horizontal">
-        <dt><code>host</code></dt><dd>Hostname or IP of the mailserver to check</dd>
-        <dt><code>dnsbl</code></dt><dd>Hostname or IP of the DNSBL server</dd>
-      </dl>
+      <pre>/api/v1/check?<strong>host</strong>=<mark>[hostname.tld]</mark>&<strong>dnsbl</strong>=<mark>[dnsbl.host.tld]</mark></pre>
+      <h5>Parameter</h5>
+      <ul>
+        <li><code>host</code>: Hostname or IP of the mailserver to check</li>
+        <li><code>dnsbl</code>: Hostname or IP of the DNSBL server</li>
+      </ul>
     </div>
     <div class="zero-clipboard"><span class="btn-clipboard">Response</span></div>
     <div class="highlight">
