5888b1e13c457a3fa557ad0fd7bd2573c16ee7be
Jonas Friedmann <j@frd.mn>
2015-05-03 17:53:41 +0200

    Update /monitor page

--- resources/views/pages/monitor.blade.php
+++ resources/views/pages/monitor.blade.php
@@ -2,9 +2,16 @@
 @section('content')
   <h1 class="page-header">Monitor</h1>
 
+  <!--
   <p>Bacon ipsum dolor amet tongue chicken beef biltong sausage pork fatback, landjaeger corned beef cow brisket. Doner capicola tenderloin spare ribs strip steak leberkas pastrami hamburger corned beef turkey sirloin chicken ham hock. Ham kielbasa sirloin short loin ball tip. Pork loin corned beef picanha, fatback alcatra ball tip shankle jerky pork belly meatball bacon. Kevin ham hock ribeye kielbasa. Boudin picanha short loin hamburger leberkas, beef prosciutto drumstick alcatra fatback pork belly jerky bresaola. Tenderloin kevin porchetta capicola bresaola doner, meatball pork belly tongue sausage pancetta picanha cow.</p>
   <p>Rump jowl turducken swine biltong chuck pastrami. Corned beef beef shankle short loin venison pork chop. Picanha t-bone venison pork belly, fatback strip steak jerky frankfurter drumstick kielbasa. Kielbasa jowl pork belly ribeye, frankfurter ham chuck ground round beef ribs turkey capicola venison picanha. Shank jowl salami ball tip picanha short ribs pork belly boudin turducken pork chop beef ribs chuck drumstick pancetta.</p>
   <p>Drumstick kielbasa kevin jowl, pancetta frankfurter alcatra bresaola pig sirloin. Pork beef ribs ham hock turkey flank hamburger prosciutto tenderloin. Ground round chuck shoulder fatback biltong meatloaf porchetta venison strip steak. Sirloin tongue corned beef swine picanha. Spare ribs beef ribs picanha corned beef jowl shank t-bone drumstick. Boudin t-bone cow, ball tip rump bacon bresaola.</p>
+  -->
+
+  <div class="text-center">
+    <h2>Work in progress</h2>
+    <p class="lead">I am currently working on the DNSBL monitor so it'll probably coming soon. Stay tuned...</p>
+  </div>
 
   <div class="clearfix"></div>
 @stop
